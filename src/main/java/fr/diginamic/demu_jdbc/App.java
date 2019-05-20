fb02f2565b156282145717ed662cd5374a8fa661
Olivier LASBLEIS <lasbleis.olivier@yahoo.fr>
2019-05-20 14:15:49 +0200

    initialisation

---
+++ src/main/java/fr/diginamic/demu_jdbc/App.java
@@ -0,0 +1,13 @@
+package fr.diginamic.demu_jdbc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
